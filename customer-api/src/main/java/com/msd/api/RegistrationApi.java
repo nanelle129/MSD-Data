cff175ab6bd909ab66ca7e3aa875ff5514993081
user <user@this.com>
2022-10-14 16:54:41 -0400

    initial commit for api classes

---
+++ customer-api/src/main/java/com/msd/api/RegistrationApi.java
@@ -0,0 +1,19 @@
+package com.msd.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationApi {
+
+	//@Autowired
+	//RegistrationRepository repo;
+	
+	//@GetMapping
+	//public Iterable<Registration> getAll() {
+	//	return repo.findAll();
+	//}
+	
+	//@GetMapping("/{RegStatus}")
+}
