30b9e786f4b9db1070f3054b49f1a429f6a7e4b6
user <user@this.com>
2022-10-14 16:22:40 -0400

    Updated package name

---
+++ customer-api/src/main/java/com/msd/MSDApplication.java
@@ -0,0 +1,13 @@
+package com.msd;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MSDApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MSDApplication.class, args);
+	}
+
+}
