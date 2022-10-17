5202ad097d6e62056b5ce810fd5d0d2e43ce969e
user <user@this.com>
2022-10-17 13:42:25 -0400

    Update domain classes with code

--- customer-api/src/main/java/com/msd/domain/Events.java
+++ customer-api/src/main/java/com/msd/domain/Events.java
@@ -1,5 +1,49 @@
 package com.msd.domain;
 
 public class Events {
-
+	long id;
+	String code;
+	String title;
+	String description;
+	
+	
+	
+	public Events() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Events(long id, String code, String title, String description) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
 }
