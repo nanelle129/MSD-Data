ed655199da37f2dda121e401368dc0973ca39335
user <user@this.com>
2022-10-17 23:50:44 -0400

    updated registrationAPI for day 2

--- customer-api/src/main/java/com/msd/api/RegistrationApi.java
+++ customer-api/src/main/java/com/msd/api/RegistrationApi.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.msd.domain.Registration;
@@ -24,74 +26,48 @@ public class RegistrationApi {
 
 	@Autowired
 	RegistrationRepository repo;
-	
+
 	@GetMapping
 	public Iterable<Registration> getAll() {
-		return repo.findall();
+		return repo.findAll();
 	}
-	
-	@GetMapping("{registrationId}")
-	public Optional<Registration> getRegistrationById(@PathVariable("registrationId")
-	long id) {
+
+	// get registration by id
+	@GetMapping("/{registrationId}")
+	public Optional<Registration> getRegistrationById(@PathVariable("registrationId") long id) {
 		return repo.findById(id);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<?> addRegistration(@RequestBody Registration newRegistration,
-			UriComponentsBuilder uri) {
-		if (newRegistration.getId() != 0) {
+	public ResponseEntity<?> addRegistration(@RequestBody Registration newRegistration, UriComponentsBuilder uri) {
+		if (newRegistration.getId() != 0 || newRegistration.getEvent_id() == null || newRegistration.getCustomer_id() == null || newRegistration.getRegistration_date() == null) {
+			// Reject we'll assign the event id
 			return ResponseEntity.badRequest().build();
 		}
-		newRegistration=repo.save(newRegistration);
-		URI location =ServletUriComponentBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(newRegistration.getId()).toUri();
-		ResponseEntity<?> response=ResponseEntity.created(location).build();
+		newRegistration = repo.save(newRegistration);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newRegistration.getId()).toUri();
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
 		return response;
 	}
-	
-	@PutMapping
-	public ResponseEntity<?> putRegistration(@RequestBody Registration newRegistration,
-			@PathVariable("registrationId") long registrationId) {
-		if (newRegistration.getId() != registrationId) {
+
+	@PutMapping("/{eventId}")
+	public ResponseEntity<?> putRegistration(
+			@RequestBody Registration newRegistration,
+			@PathVariable("eventId") long eventId) 
+	{
+		if (newRegistration.getEvent_id() == null || newRegistration.getCustomer_id() == null ) { //|| newRegistration.getRegistration_date() == null) {
 			return ResponseEntity.badRequest().build();
 		}
-		newRegistration=repo.save(newRegistration);
-		
+		newRegistration = repo.save(newRegistration);
 		return ResponseEntity.ok().build();
+	}	
+	
+	//delete registration
+	@DeleteMapping("/{eventId}")
+	public ResponseEntity<?> deleteRegistrationById(@PathVariable("eventId") long id) {
+		repo.deleteById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
-	
-	@DeleteMapping("/{registrationId}")
-	public ResponseEntity<?> deleteRegistrationById(@PathVariable("registrationId") long id) {
-		return null;
-	}
-	
-	/*ArrayList<Registration> registrationList = new ArrayList<Registration>();
-	
-	@SuppressWarnings("deprecation")
-	public RegistrationApi() {
-		Registration r1 = new Registration(1L, "1", "2", new Date(), "please email me the event details");
-		Registration r2 = new Registration(2L, "2", "2", new Date(), "send transportation and hotel booking");
-		Registration r3 = new Registration(3L, "3", "3", new Date(), "defer payments for a week");
-		
-		registrationList.add(r1);
-		registrationList.add(r2);
-		registrationList.add(r3);
-	}
-	
-	@GetMapping
-	public Collection<Registration> getAll() {
-		return this.registrationList;
-	}
-	
-	@GetMapping("/{registrationId}")
-	public Registration getCustomerById(@PathVariable("registrationId") long id ) {
-		Registration registration = null;
-		for (int i = 0; i < registrationList.size(); i++) {
-			if (registrationList.get(i).getId() == id) {
-				registration = registrationList.get(i);
-			}
-		}
-		return registration;
-	}*/
 	
 }
