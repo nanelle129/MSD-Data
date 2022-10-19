579f5c9faab7ff40495980f78596db543f65ccd9
user <user@this.com>
2022-10-19 16:50:07 -0400

    Added AuthFiler and Dependencies

---
+++ customer-api/build/resources/main/data.sql
@@ -0,0 +1,25 @@
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Austin', 'pass', 'austin@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Michael', 'pass', 'michael@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Timothy', 'pass', 'timothy@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Chris', 'pass', 'chris@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Corey', 'pass', 'corey@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Jay', 'pass', 'jay@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Phu', 'pass', 'phu@bah.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Dipendra', 'pass', 'dipendra@bah.com');
+
+insert into EVENTS (EVENT_CODE, EVENT_TITLE, DESCRIPTION) VALUES ('CAN118','Hello World!','Panel discussion on the history of Software Development');
+insert into EVENTS (EVENT_CODE, EVENT_TITLE, DESCRIPTION) VALUES ('DDA629','Angular Trainging Course','Five day introductory Angular workshop');
+insert into EVENTS (EVENT_CODE, EVENT_TITLE, DESCRIPTION) VALUES ('WND129','Dinner Under the Stars','BAH Employee dining event');
+insert into EVENTS (EVENT_CODE, EVENT_TITLE, DESCRIPTION) VALUES ('PCG926','Team, Ideas, Results','BAH Employee Team Building Exercise');
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values (1,1, '2022-10-20 00:00:00.0', 'Please send email confirmation')
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values (2,2, '2022-10-20 00:00:00.0', 'Please send email confirmation')
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values (3,3, '2022-10-20 00:00:00.0', 'Please send email confirmation')
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values (4,4, '2022-10-20 00:00:00.0', 'Please send email confirmation')
---
+++ project-mcc-data-service-crud/src/main/java/com/webage/filter/AuthFilter.java
@@ -0,0 +1,60 @@
+package com.webage.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+import com.webage.util.JWTHelper;
+//import com.webage.util.JWTUtil;
+
+
+@Component
+public class AuthFilter implements Filter{
+
+	//JWTUtil jwtUtil = new JWTMockUtil();
+	JWTHelper jwtUtil = new JWTHelper();
+	private String api_scopes = "com.webage.data.apis";
+	private String auth_scopes = "com.webage.auth.apis";
+	//private String api_scope = "com.api.customer.r";
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String uri = req.getRequestURI();
+		if (uri.startsWith("/token")) {
+			// continue on to get-token endpoint
+			chain.doFilter(request, response);
+			return;
+		}
+		else {
+			// check JWT token
+			String authheader = req.getHeader("authorization");
+			System.out.println("Check JWT Token from header [" + authheader + "]");
+			if (authheader != null && authheader.length() > 7
+				&& authheader.startsWith("Bearer")) {
+				String jwt_token = authheader.substring(7, authheader.length());
+				if (jwtUtil.verifyToken(jwt_token)) {
+					String request_scopes = jwtUtil.getScopes(jwt_token);
+					if (request_scopes.contains(api_scopes) ||request_scopes.contains(auth_scopes) ) {
+						// continue on to api
+						chain.doFilter(request, response);
+						return;
+					}
+				}
+			}
+		}
+		// reject request and return error instead of data
+		res.sendError(HttpServletResponse.SC_FORBIDDEN, 
+				"failed authentication");
+		}
+}
